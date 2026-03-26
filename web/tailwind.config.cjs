f81fbc47b1d71fd6e29214348675a496375950bb
Shchipankina Alina <alina2002malina@mail.ru>
2026-03-26 21:51:56 +0300

    replacing the styles version

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,13 +1,27 @@
-import daisyui from "daisyui"
+// import daisyui from "daisyui"
+
+// export default {
+//   content: [
+//     "./resources/**/*.blade.php",
+//     "./resources/**/*.js",
+//     "./resources/**/*.php",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [daisyui],
+// }
 
 export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.php",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
   ],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [],
 }
